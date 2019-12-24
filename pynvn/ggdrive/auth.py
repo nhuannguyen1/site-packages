839282d969898db25dad5adbb0a19668435617a6
=Nhuan <=nguyenvannhuan90123@gmail.com>
2019-12-24 14:06:27 +0700

    all

--- pynvn/ggdrive/auth.py
+++ pynvn/ggdrive/auth.py
@@ -1,12 +1,13 @@
 from __future__ import print_function
 import httplib2
 import os
-
+from google_auth_oauthlib.flow import InstalledAppFlow
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-
+from google.auth.transport.requests import Request
+import pickle
 try:
     import argparse
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
@@ -15,9 +16,9 @@ except ImportError:
 
 
 class auth:
-    def __init__(self,SCOPES,
-                CLIENT_SECRET_FILE,
-                APPLICATION_NAME):
+    def __init__(self,SCOPES ,
+                CLIENT_SECRET_FILE =None ,
+                APPLICATION_NAME = None):
         self.SCOPES = SCOPES
         self.CLIENT_SECRET_FILE = CLIENT_SECRET_FILE
         self.APPLICATION_NAME = APPLICATION_NAME
@@ -55,3 +56,25 @@ class auth:
                                         store)
             print('Storing credentials to ' + credential_path)
         return credentials
+    def creds(self):
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                        'client_secret.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        print ("creds",creds)
+        return creds
+    
