5abfd92baea22675c07381daf3198242c9a8c9bf
Nguyen Van Nhuan <45336894+nhuannguyen1@users.noreply.github.com>
2020-06-10 20:27:20 +0700

    all

--- pynvn/excel/hdata.py
+++ pynvn/excel/hdata.py
@@ -47,20 +47,24 @@ class hexcel:
         # list all sheet name from file path 
         lsheetnames = relistsheet(self.fpath)
         lsheet = self.listsheetnameinexsting(listnames=lsheetnames,wsheet_AZ30=wsheet_AZ30)
-        for i in range(6,49):
+        for i in range(6,self.mrow):
             sumvalue = ""
             for hmname in lsheet:
                 valuesum = self.valuecolsheet(i = i ,hmname=hmname)
                 sumvalue = sumvalue + "+" +  valuesum
             self.wsheet.cell(row=i,column=8).value =  "=" + sumvalue 
     
-    def valuecolsheet (self,i = 1,hmname = None ):
+    def valuecolsheet (self,i = 1,hmname = None, namefiter = "62"):
         """fomulas for column follow index"""
-
+        valuechek = self.wsheet.cell(row=i,column=3).value 
         pfile = repathlinkexcel(dpath=self.dpath,
                                 namefile=self.namefile,
                                 namesheet=hmname)
-        valueeee = 'SUMIF({0}!$BC:$BC,C{1},{0}!$CA:$CA) + SUMIF({0}!$BC:$BC,C{1},{0}!$CB:$CB)'.format(pfile,i) + "+" + 'SUMIF({0}!$BC:$BC,C{1},{0}!$CA:$CA) + SUMIF({0}!$BC:$BC,C{1},{0}!$CA:$CA)'.format(pfile,i)
+        if str(valuechek)[:2] != namefiter:
+
+            valueeee = 'SUMIF({0}!$BC:$BC,C{1},{0}!$CA:$CA) + SUMIF({0}!$BC:$BC,C{1},{0}!$CB:$CB)'.format(pfile,i) + "+" + 'SUMIF({0}!$BC:$BC,C{1},{0}!$CA:$CA) + SUMIF({0}!$BC:$BC,C{1},{0}!$CA:$CA)'.format(pfile,i)
+        else:
+            valueeee = 'SUMIF({0}!$BC:$BC,C{1},{0}!$CA:$CA) + SUMIF({0}!$BC:$BC,C{1},{0}!$CC:$CC)'.format(pfile,i) + "+" + 'SUMIF({0}!$BC:$BC,C{1},{0}!$CA:$CA) + SUMIF({0}!$BC:$BC,C{1},{0}!$CD:$CD)'.format(pfile,i)
 
         return valueeee
 
@@ -69,7 +73,7 @@ class hexcel:
         return [wsheet_AZ30.cell(row=3,column=k).value for k in range (10,self.mcol) if wsheet_AZ30.cell(row=3,column=k).value in listnames]
 
     def returnlistvaluebycolumnindex (self):
-        
+
         return [cell.value for row in ws.iter_rows('C{}:C{}'.format(ws.min_row,ws.max_row)) for cell in row ]
 
         
