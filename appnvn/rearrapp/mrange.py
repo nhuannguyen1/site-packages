40706a761ca27d48b85c9bd920ad2233062708cc
Nguyen Van Nhuan <45336894+nhuannguyen1@users.noreply.github.com>
2020-08-08 13:50:54 +0700

    all

--- appnvn/rearrapp/mrange.py
+++ appnvn/rearrapp/mrange.py
@@ -1,22 +1,21 @@
 from pynvn.excel.copy_move_paste import co_paste_move_range
-def mrange(sheet_name, wb):
+def hsheet_range(sheet_name, wb,range_copy,range_paste,clear_rcopy_after_copy = True):
     """ copy move paste range """
     if sheet_name == "active":
         ac_sheet = wb.sheets.active
         co_paste_move_range(sheet_copy=ac_sheet,
                         sheet_des=ac_sheet,
                         range_copy=range_copy,
-                        range_paste=range_des,
-                        clear_rcopy_after_copy=true
+                        range_paste=range_paste,
+                        clear_rcopy_after_copy=clear_rcopy_after_copy
                         )
     else:
         for sheetname in listsheet_by_wb(wb):
             if sheet_name in sheetname:
                 wsheet = wb.sheets[sheetname]
-                o_paste_move_range(sheet_copy=wsheet,
+                co_paste_move_range(sheet_copy=wsheet,
                                 range_copy=range_copy,
                                 sheet_des=wsheet,
-                                range_paste=range_des,
-                                clear_rcopy_after_copy=True
+                                range_paste=range_paste,
+                                clear_rcopy_after_copy=clear_rcopy_after_copy
                                 )
-                            
--- appnvn/rearrapp/rearr_range.py
+++ appnvn/rearrapp/rearr_range.py
@@ -5,6 +5,7 @@ from pynvn.list.flist import filterlistbylstr
 from pynvn.excel.copy_move_paste import co_paste_move_range
 from pynvn.excel import activesheet,open_wb_byxl,listsheet_by_wb
 from pynvn import dict_from_csv2col
+from appnvn.rearrapp.mrange import hsheet_range
 class rearr_range:
     def __init__(self,lpath_excel =[],
                 pathconf = None,
@@ -28,33 +29,14 @@ class rearr_range:
                     if lfun == "move_range":
                         mrange = self.__dictconf["move_range"]
                         Value_Conf_Loc = self.__dictconf["sub_move_range_value_conf_loc"]
-                        sheet_name = self.__dictconf["sub_sheetname"]
-                        range_copy = self.__dictconf["sub_range_copy"]
-                        range_des = self.__dictconf["sub_range_des"]
-                        if mrange == "yes":
-                            if sheet_name == "active":
-                                ac_sheet = wb.sheets.active
-                                co_paste_move_range(sheet_copy=ac_sheet,
-                                                    sheet_des=ac_sheet,
-                                                    range_copy=range_copy,
-                                                    range_paste=range_des,
-                                                    clear_rcopy_after_copy=true
-                                                    )
-                            else:
-                                for sheetname in listsheet_by_wb(wb):
-                                    if sheet_name in sheetname:
-                                        wsheet = wb.sheets[sheetname]
-                                        co_paste_move_range(sheet_copy=wsheet,
-                                                            range_copy=range_copy,
-                                                            sheet_des=wsheet,
-                                                            range_paste=range_des,
-                                                            clear_rcopy_after_copy=True
-                                                            )
-                            
-                            """
-                            else:
-                                messagebox.showerror("Error","recheck name key {0} in directory {1}".format("sub_sheetname",self.__pathconf))
-                            """
+                        sheet_name = self.__dictconf["sub_move_range_sheetname"]
+                        range_copy = self.__dictconf["sub_move_range_range_copy"]
+                        range_des = self.__dictconf["sub_move_range_range_des"]
+                        hsheet_range(sheet_name=sheet_name,wb=wb) if mrange == "yes" else False
+                    elif lfun == "mrange_by_cell":
+                        sheet_name= self.__dictconf["sub_mrange_by_cell_sheetname"]
+                        range_copy = self.__dictconf["sub_mrange_by_cell_loc_range_copy"]
+                        range_des = self.__dictconf["sub_move_range_range_des"]
                         
                 wb.save()
                 wb.app.quit()       
